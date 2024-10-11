df5ef43d1a714c950bb1ce1babc1c8b0a1d99235
FurkanYOLCU <furkanyolcu566@gmail.com>
2024-10-11 22:58:51 +0300

    Proje dosyası ekle.

---
+++ Virtual_shopping/Models/Context.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Virtual_Shopping.Models
+{
+    public class Context
+    {
+        #region FurkanDB
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=FURKAN; database=ShoppingDB; integrated security=true; TrustServerCertificate=true");
+        //}
+        #endregion
+
+        #region SadıkDB
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("server=localhost; database=ShoppingDB; integrated security=true; TrustServerCertificate=True;");
+        //}
+        #endregion
+
+    }
+}
+
